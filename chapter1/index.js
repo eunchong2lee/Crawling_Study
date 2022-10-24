2c00dd3c1893a1aa0720790b3bf6c3ca248397d2
eunchong2lee <tlfqjrjs1234@naver.com>
2022-10-24 16:28:00 +0900

    [Complete] Study

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,9 +1,10 @@
 // npm csv-parser 
-// csv파일 parsing
 const csv = require('csv-parser')
 const fs = require('fs')
+// npm xlsx
+const xlsx = require('xlsx');
 
-
+// csv파일 parsing
 const parser = async () => {
     const parsing = await new Promise((resolve, reject) => {
         const results = [];
@@ -21,4 +22,39 @@ const parser = async () => {
     });
 }
 
-parser();
+// parser();
+
+
+// xlsx-1
+const xlsx_parser1 = () => {
+    const workbook = xlsx.readFile('xlsx/data.xlsx');
+    // console.log(Object.keys(workbook.Sheets)); 
+    const ws = workbook.Sheets.영화목록;
+    const records = xlsx.utils.sheet_to_json(ws); 
+    for (const [i, r] of records.entries()) {
+    console.log(i, r);
+    }
+}
+
+// xlsx_parser1();
+
+// xlsx-2
+
+const xlsx_parser2 = () => {
+    const workbook = xlsx.readFile('xlsx/data.xlsx');
+
+    const ws = workbook.Sheets[workbook.SheetNames[0]];
+    console.log(ws['!ref']);
+    ws['!ref'] = ws['!ref'].split(':').map((v, i) => {
+    if (i === 0) { return 'A2'; }
+        return v;
+    }).join(':');
+
+    const records = xlsx.utils.sheet_to_json(ws, { header: 'A'});
+    console.log(records);
+
+    for (const [i, r] of records.entries()) {
+        console.log(i, r);
+    }
+}
+xlsx_parser2();
