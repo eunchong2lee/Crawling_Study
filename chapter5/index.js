15a1ee11aa42150e31041bdddff7adda8809e13d
eunchong2lee <tlfqjrjs1234@naver.com>
2022-10-26 15:50:24 +0900

    [Complete] Study

--- chapter5/index.js
+++ chapter5/index.js
@@ -1,1 +1,41 @@
-//
+const puppeteer = require('puppeteer'); 
+
+const crawler = async () => {
+    try {
+      const browser = await puppeteer.launch({ headless: false });
+      const page = await browser.newPage();
+      await page.goto('https://unsplash.com');
+      let result = [];
+      while (result.length <= 30) {
+        const srcs = await page.evaluate(() => {
+          window.scrollTo(0, 0);
+          let imgs = [];
+          const imgEls = document.querySelectorAll('.nDTlD'); 
+          if (imgEls.length) {
+            imgEls.forEach((v) => {
+              let img = v.querySelector('img._2zEKz'); 
+              if (img && img.src) {
+                imgs.push(img.src);
+              }
+              v.parentElement.removeChild(v);
+            });
+          }
+          
+          window.scrollBy(0, 100);
+          setTimeout(() => {
+            window.scrollBy(0, 200);
+          }, 500);
+          return imgs;
+        });
+        result = result.concat(srcs);
+        await page.waitForSelector('figure');
+      }
+      console.log(result);
+      await page.close();
+      await browser.close();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  
+  crawler();
